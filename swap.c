98949d0fdd6d5daafc2abf798a09a863a25df6e8
VitalyTen <vtenigin@student.42.us.org>
2017-01-05 17:34:32 -0800

    swap.c

---
+++ swap.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vtenigin <vtenigin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/05 17:28:05 by vtenigin          #+#    #+#             */
+/*   Updated: 2017/01/05 17:34:04 by vtenigin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+sa : swap a - swap the first 2 elements at the top of stack a.
+				Do nothing if there is only one or no elements).
+sb : swap b - swap the first 2 elements at the top of stack b.
+				Do nothing if there is only one or no elements).
+ss : sa and sb at the same time.
+*/
+
+void	sa(t_env *env)
+{
+	int	tmp;
+
+	if (env->lena > 1)
+	{
+		tmp = env->a[0];
+		env->a[0] = env->a[1];
+		env->a[1] = tmp;
+	}
+}
+
+void	sb(t_env *env)
+{
+	int	tmp;
+
+	if (env->lenb > 1)
+	{
+		tmp = env->b[0];
+		env->b[0] = env->b[1];
+		env->b[1] = tmp;
+	}
+}
+
+void	ss(t_env *env)
+{
+	sa(env);
+	sb(env);
+}
