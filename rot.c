3159cb16e9b461ae2cc7a1d48d9110d757f124d1
VitalyTen <vtenigin@student.42.us.org>
2017-01-05 18:10:28 -0800

    rot.c

---
+++ rot.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rot.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vtenigin <vtenigin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/05 17:57:47 by vtenigin          #+#    #+#             */
+/*   Updated: 2017/01/05 18:07:43 by vtenigin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+ra : rotate a - shift up all elements of stack a by 1.
+				The first element becomes the last one.
+rb : rotate b - shift up all elements of stack b by 1.
+				The first element becomes the last one.
+rr : ra and rb at the same time.
+*/
+
+void	ra(t_en *env)
+{
+	int	i;
+	int	tmp;
+
+	if (env->lena > 1)
+	{
+		i = -1
+		tmp = env->a[0];
+		while (++i < env->lena - 1)
+			env->a[i] = env->a[i + 1];
+		env->a[++i] = tmp;
+	}
+}
+
+void	rb(t_en *env)
+{
+	int	i;
+	int	tmp;
+
+	if (env->lenb > 1)
+	{
+		i = -1
+		tmp = env->b[0];
+		while (++i < env->lenb - 1)
+			env->b[i] = env->b[i + 1];
+		env->b[++i] = tmp;
+	}
+}
+
+void rr(t_en *env)
+{
+	ra(env);
+	rb(env);
+}
